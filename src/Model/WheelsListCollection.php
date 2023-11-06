650132020befae2f00b5ee1e19f0d8546ba7dfb1
ColdSiMENS13 <simon.simonchikdima@gmail.com>
2023-11-06 22:24:32 +0300

    add wheel size

---
+++ src/Model/WheelsListCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model;
+
+class WheelsListCollection
+{
+    private array $items;
+
+    public function __construct(array $items)
+    {
+        $this->items = $items;
+    }
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+}
