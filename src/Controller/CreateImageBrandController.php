2dd09ecdfeca72165dfe3fc0d67589a83f8ff3d0
ColdSiMENS13 <simon.simonchikdima@gmail.com>
2023-12-20 21:19:05 +0300

    A

--- src/Controller/CreateImageBrandController.php
+++ src/Controller/CreateImageBrandController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,12 +17,10 @@ class CreateImageBrandController extends AbstractController
     }
 
     #[Route(path: '/image/upload', name: 'upload')]
-    public function uploadImage(): Response
+    public function uploadImage(Request $request)
     {
-        $name = $_FILES['image']['name'];
-        $tmp_name = $_FILES['image']['tmp_name'];
-        move_uploaded_file($tmp_name, "../Uploads" . $name);
-        return $this->render('imageUpload.html.twig');
+        $image = $request->files->get('image');
+        dd($image);
     }
 
 }
